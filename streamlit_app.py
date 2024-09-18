b4a3e1aaf1d61fa46cbc4a0b6d9743f60d09902b
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-09-18 20:02:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,20 @@ if btn:
 
 
 # app1
+area =None
 st.header("calculate area")
 choose=st.selectbox('choose the shape',['circle'],['rectangle'])
 if choose=='circle':
     r=st.number_input('enter the radius',min_value=1.0,max_value=100)
+    area=3.14*r*r
     btn=st.button('calculate')
-    if btn:
-      area=3.14*r*r
-      st.write(f'area of circle is {area}')
+
 elif choose=='rectangle':
     w=st.number_input('enter the radius',min_value=1.0,max_value=100)
     h=st.number_input('enter the radius',min_value=1.0,max_value=100)
     area=h*w
     btn=st.button('calculate')
     if btn:
+      st.write(f'area  is {area}')
+    
+      
