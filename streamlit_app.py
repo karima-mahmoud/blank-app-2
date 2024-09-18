3a7bf2e0341015885a6da0acc5395999d901a5a6
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-09-18 21:32:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,21 @@ if file is not None:
         st.write(df[:num_row][names_columns])
     else:
         st.write(df[:num_row])
+
+
+num_row = st.slider('choose num rows', min_value=1, max_value=len(df), step=1)
+names_column = st.multiselect('choose names of columns', df.columns.to_list())
+st.write(df[:num_row][names_column])
+
+if names_column:
+    st.write(df[:num_row][names_column])
+else:
+    st.write(df[:num_row])
+
+num_col = df.select_dtypes(include='number').columns.to_list()
+
+color = st.selectbox('choose color', df.columns.to_list())
+
+fig = px.scatter(df, x=x_col, y=y_col, color=color)
+st.plotly_chart(fig)
+
